c3fec2d36da2b7f7de34f170356ba2a8377c8541
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-16 14:41:05 -0800

    more playing with js typing issues

--- src.js
+++ src.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+var x = 133;
+console.log(x);
+
+x = "ehsan";
+console.log(x);
+
+y = parseInt(x);
+console.log(y++);
+
+const obj = {
+  name: "ehsan",
+  age: 27,
+};
+
+console.log(obj["birthday"]);
