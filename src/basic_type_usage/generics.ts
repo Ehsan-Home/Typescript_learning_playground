5d07c8052850605025640d8ae8cf52ed3ac86f25
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-18 11:30:00 -0800

    Defining multi generic types

--- src/basic_type_usage/generics.ts
+++ src/basic_type_usage/generics.ts
@@ -18,9 +18,25 @@ let opel:LuxuryCar = {
     speed:200
 }
 
+function returnTheSame2<T1>(variable:T1) {
+    return variable
+}
+
 function returnTheSame<T1 extends T2,T2>(variable:T1):T2 {
     return variable
 }
+
+function newConsole<T1,T2>(variable1:T1,variable2:T2){
+    console.log(variable1)
+    console.log(variable2)
+}
+
+newConsole("ehsan",123)
+newConsole<string, number>("ehsan", 123)
+
+returnTheSame2("ehsan")
+returnTheSame2(123)
+returnTheSame2(["a","b","v"])
 
 let res = returnTheSame<LuxuryCar, Vehicle>(opel)
 let res2 = returnTheSame(123)
